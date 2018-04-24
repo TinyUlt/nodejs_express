7581891f5cf330e9ddbf1ae7ee7d37146a41a7ab
tinyult <tinyUlt@icloud.com>
2018-04-24 18:58:13 +0800

    a

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ function find(collectionName, startTime,endTime,  response){
          response.end(JSON.stringify(result));
     });
 }
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
 
     var host = server.address().address;
     var port = server.address().port;
